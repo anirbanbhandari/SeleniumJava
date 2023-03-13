f2d0ed178b2c7318e8a7ef1b7b01e8b6c619024e
Anirban <anirbanbhandari1@gmail.com>
2023-03-13 10:39:08 +0530

    Day02
    Window operations and Screenshot

--- src/day01/Program1.java
+++ src/day01/Program1.java
@@ -3,13 +3,16 @@ package day01;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Program1 {
 
 	public static void main(String[] args) {
 		
 		WebDriver driver;
-		driver = new ChromeDriver();
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--remote-allow-origins=*");
+		driver = new ChromeDriver(options);
 		
 		driver.get("https://demo.automationtesting.in/Register.html");
 		driver.findElement(By.xpath("//input[@placeholder='First Name']")).sendKeys("Test");
